ac7787e5f9ff36f252df3c0ee5c01946c3106bc6
voducmanh99 <voducmanh963@gmail.com>
2020-09-08 08:41:53 +0700

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 	showFullContent();
+	initSlide();
 })
 
 
@@ -10,4 +11,26 @@ function showFullContent() {
 		$(this).prev('.full-content').toggleClass('active')
 		$(this).slideUp();
 	})
+}
+
+function initSlide() {
+	let productSlide = new Swiper(".the-workshop-product-2 .swiper-container", {
+		// slidesPerView: 3,
+		// slidesPerColumn: 2,
+		autoplay: {
+			delay: 4500,
+		},
+		spaceBetween: 30,
+		speed: 500,
+		loop: true,
+		navigation: {
+			nextEl: ".the-workshop-product-2 .button-next",
+			prevEl: ".the-workshop-product-2 .button-prev",
+		},
+		pagination: {
+			el: ".the-workshop-product-2 .swiper-pagination",
+			type: "bullets",
+			clickable: true,
+		},
+	});
 }
